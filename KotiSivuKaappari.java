0fb3153f4ddf9150897203fdb05fe1ddc7ee1fcf
TuoHal <36262254+TuoHal@users.noreply.github.com>
2019-05-13 09:52:07 +0300

    Add files via upload

---
+++ KotiSivuKaappari.java
@@ -0,0 +1,29 @@
+package kotisivukaappari;
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+/**
+ *
+ * @author tuomas.halonen
+ */
+public class KotiSivuKaappari {
+
+
+    public static void main(String[] args) {
+        // Creation of variables
+        
+      String content = null;
+URLConnection connection = null;
+try {
+  connection =  new URL("https://vita.fi/laboratoriokasikirja/tutkimus/270").openConnection();
+  Scanner scanner = new Scanner(connection.getInputStream());
+  scanner.useDelimiter("\\Z");
+  content = scanner.next();
+  scanner.close();
+}catch ( Exception ex ) {
+    ex.printStackTrace();
+}
+System.out.println(content);
+    }
+    
+}
